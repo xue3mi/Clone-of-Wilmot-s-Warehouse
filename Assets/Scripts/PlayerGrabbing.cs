b801a3416a5ecb43f003b3f687d2da365ebd134b
Psycanic <psycanic@gmail.com>
2025-04-13 18:14:19 -0400

    idk

--- Assets/Scripts/PlayerGrabbing.cs
+++ Assets/Scripts/PlayerGrabbing.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerGrabbing : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
--- Assets/Scripts/SelectManager.cs
+++ Assets/Scripts/SelectManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class movementcontroller : MonoBehaviour
+public class SelectManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
