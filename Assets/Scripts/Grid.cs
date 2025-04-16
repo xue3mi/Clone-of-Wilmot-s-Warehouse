82632f413ad0a4f15bfd1846a8985867b5ac3504
Psycanic <psycanic@gmail.com>
2025-04-16 11:27:57 -0400

    summer's gridobj storage

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class Grid: MonoBehaviour
+/*public class Grid: MonoBehaviour
 {
     public static List<GridObject> gridObjects;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,3 +16,71 @@ public class Grid: MonoBehaviour
         
     }
 }
+*/
+
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    // object coordinate (x, y)
+    public static Grid Instance;
+
+    // store all detacted target blocks & their coordinates in lists
+    public List<Vector2Int> allPositions = new List<Vector2Int>();
+    private List<GridObject> allGridObjects = new List<GridObject>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void UpdateGridObject(GridObject gridObject)
+    {
+        if (!allGridObjects.Contains(gridObject))
+        {
+            // if not added into list, add for the first time
+            allGridObjects.Add(gridObject);
+        }
+        // Every update, refresh the coordinate list
+        RefreshAllPositions();
+    }
+
+    void RefreshAllPositions()
+    {
+        // delete old coordinates if updated
+        allPositions.Clear();
+
+        foreach (GridObject obj in allGridObjects)
+        {
+            // add the newest coordinates to list
+            allPositions.Add(obj.gridPosition);
+        }
+    }
+
+
+
+    // test
+    private float timer = 0f;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // Every sec update the coordinates
+        if (timer >= 1f)
+        {
+            timer = 0f;
+
+            // print all coordinates positions
+            Debug.Log("All blocks' coordinate£º");
+            foreach (Vector2Int pos in allPositions)
+            {
+                Debug.Log(pos);
+            }
+        }
+    }
+
+
+}
+
+    
--- Assets/Scripts/GridObject.cs
+++ Assets/Scripts/GridObject.cs
@@ -1,25 +1,59 @@
 using UnityEngine;
 
-public class GridObject : MonoBehaviour
+public class GridObject: MonoBehaviour
 {
 
     public Vector2 gridPositio = Vector2.zero;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+ 
+   
+    
+
+    
+    public Vector2Int gridPosition;
+
+    private Vector2Int lastPosition;
+
     void Start()
     {
-        
+        //calculate world position(coordinate) according to its own position
+        Vector2 myWorldPos = transform.position;
+        gridPosition = new Vector2Int(Mathf.RoundToInt(myWorldPos.x), Mathf.RoundToInt(myWorldPos.y));
+        lastPosition = gridPosition;
+
+        // tell GridManager coordinate
+        Grid.Instance.UpdateGridObject(this);
     }
 
-    // Update is called once per frame
+    // automatic update if coordinate changed
     void Update()
     {
         //if not connected to wilmot && moving
-            //GridSnap()
+        //GridSnap()
+
+
+
+        Vector2Int currentPosition = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.y));
+
+        if (currentPosition != lastPosition)
+        {
+            gridPosition = currentPosition;
+            lastPosition = currentPosition;
+            Grid.Instance.UpdateGridObject(this);
+        }
     }
 
-    
+    // if coordinate changed or updated£¬call this function to tell GridManager
+    public void SetGridPosition(int x, int y)
+    {
+        gridPosition = new Vector2Int(x, y);
+        Grid.Instance.UpdateGridObject(this);
+    }
     private void GridSnap()
-    { 
+    {
         //transform coordinat to grid coordinates
     }
+    // object coordinate (x, y)
 }
+
+
+
