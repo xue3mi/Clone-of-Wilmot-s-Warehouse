e07e1c51f6bf51c636a6a46007cf73ed123d12de
Summer_Lin <xue3mi@gmail.com>
2025-04-23 11:56:39 -0400

    Fixed some illumination problem
    Changed diameter to radius

--- Assets/Scripts/LightningTile.cs
+++ Assets/Scripts/LightningTile.cs
@@ -10,26 +10,30 @@ public class LightningTile : MonoBehaviour
             spriteRenderer = GetComponent<SpriteRenderer>();
             //attach GridObject component to all blocks Prefab
             gridObject = GetComponent<GridObject>();
-    }
+        }
 
         public void UpdateLight(Vector2Int playerGridPos)
         {
-            int distance = Mathf.Abs(playerGridPos.x - gridObject.gridPosition.x) + Mathf.Abs(playerGridPos.y - gridObject.gridPosition.y);
+            int dx = Mathf.Abs(gridObject.gridPosition.x - playerGridPos.x);
+            int dy = Mathf.Abs(gridObject.gridPosition.y - playerGridPos.y);
 
-            //illuminate distance, white
-            if (distance <= 5)
+            float euclideanDistance = Vector2.Distance(gridObject.gridPosition, playerGridPos);
+
+            // white sqaure radius=2
+            if (dx <= 2 && dy <= 2)
             {
                 spriteRenderer.color = Color.white;
             }
-            // within 7 tiles, grey
-            else if (distance <= 7)
+            // grey circle radius=3.5
+            else if (euclideanDistance <= 3.5f)
             {
                 spriteRenderer.color = new Color(0.8f, 0.8f, 0.8f);
             }
-            // outside 7 tiles, black
+            // black sqaure radius=3.5
             else
             {
                 spriteRenderer.color = Color.black;
             }
         }
-    }
+
+}
